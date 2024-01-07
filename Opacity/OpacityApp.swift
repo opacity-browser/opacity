d3cf63e0d3c3d5182b095bd16db88f8703c2a533
CheolJe <me@falsy.me>
2024-01-07 11:22:46 +0900

    init

---
+++ Opacity/OpacityApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpacityApp.swift
+//  Opacity
+//
+//  Created by Falsy on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct OpacityApp: App {
+  var body: some Scene {
+    WindowGroup {
+      Text("Hello world")
+    }
+  }
+}
