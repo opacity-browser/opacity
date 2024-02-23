5fd25db283d2997ef0fd77cc8751dc4b0d8cdddb
CheolJe <me@falsy.me>
2024-02-23 19:32:26 +0900

    feat: modify tab switching method

--- Model/Browser.swift
+++ Model/Browser.swift
@@ -13,37 +13,17 @@ final class Browser: ObservableObject, Identifiable {
   var windowNumber: Int?
   @Published var tabs: [Tab] = []
   @Published var activeTabId: UUID?
-  @Published var snapshot: NSImage?
   
   private var currentWorkItem: DispatchWorkItem?
   
   func updateActiveTab(tabId: UUID, webView: WKWebView) {
-    currentWorkItem?.cancel()
-    takeSnapshot(webView: webView)
-    
-    let workItem = DispatchWorkItem { [weak self] in
-      self?.activeTabId = tabId
-      DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-        self?.snapshot = nil
-      }
-    }
-    
-    currentWorkItem = workItem
-    DispatchQueue.main.asyncAfter(deadline: .now() + 0.08, execute: workItem)
-  }
-  
-  func takeSnapshot(webView: WKWebView) {
-    let configuration = WKSnapshotConfiguration()
-
-    webView.takeSnapshot(with: configuration) { image, error in
-      guard let image = image, error == nil else {
-        print("err")
-        self.snapshot = nil
-        return
-      }
-      print("snapshot")
-      self.snapshot = image
-    }
+    self.activeTabId = tabId
+//    currentWorkItem?.cancel()
+//    
+//    let workItem = DispatchWorkItem { [weak self] in
+//      self?.activeTabId = tabId
+//    }
+//    DispatchQueue.main.asyncAfter(deadline: .now() + 0.08, execute: workItem)
   }
   
   func newTab(_ url: URL = DEFAULT_URL) {
