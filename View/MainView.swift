c82dc8fef48b632fa374f1749423874fddff38f5
CheolJe <me@falsy.me>
2024-03-04 12:00:03 +0900

    feat: modify detailed styles

--- View/MainView.swift
+++ View/MainView.swift
@@ -18,7 +18,8 @@ struct MainView: View {
             ForEach(Array(browser.tabs.enumerated()), id: \.element.id) { index, tab in
               if let activeId = browser.activeTabId {
                 WebNSView(browser: browser, tab: browser.tabs[index])
-                  .offset(y: tab.id == activeId ? 0 : geometry.size.height)
+                  .offset(y: tab.id == activeId ? 0 : geometry.size.height + 1)
+                  .frame(height: geometry.size.height + 1)
               }
             }
           }
